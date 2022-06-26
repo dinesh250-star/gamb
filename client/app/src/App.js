c9e1fa70d1b1d2ff7da3b5cfbd576928bb7ce1cd
Dinesh Lakshmanan <dineshlakshman2001@gmail.com>
2022-06-26 18:36:38 +0530

    cleaning code

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -4,16 +4,9 @@ import Axios from "axios";
 import Login from "./Components/Login";
 
 function App() {
-  const click = () => {
-    Axios.post("http://localhost:3001/create", {
-      rno: 2,
-    });
-    console.log("click");
-  };
   return (
     <div className="App">
       <Login />
-      <button onClick={click}>click</button>
     </div>
   );
 }
